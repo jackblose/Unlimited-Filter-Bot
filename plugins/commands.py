f518403e2fa525f1752c3c4e90452578e0575428
waly <85253290+jackblose@users.noreply.github.com>
2021-07-07 21:18:19 +0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Temanesia", url="https://t.me/temanesiaonline"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Channel Kata", url="https://t.me/racauanhatii"),
+                    InlineKeyboardButton("Bot Info", url="https://t.me/wysupportinfo")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://kepo lu ye wkwk")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
